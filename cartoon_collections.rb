87cf358f9ac801b499fc585e69094634225ba556
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-13 09:02:36 -0400

    Built all required methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  list = []
+  dwarves.each_with_index do |person, index|
+    list << "#{index + 1}. #{person}"
+  end
+  print list
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |calls|
+   calls.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  if calls_long.length > 4
+    true
+  else
+   false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
